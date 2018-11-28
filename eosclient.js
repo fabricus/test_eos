a32282608680718e1b4ff723af87259793a827da
Pelisboule <fabienpelissier@Fabiens-iMac.local>
2018-11-28 18:36:32 +0100

    Update eosclient.js
    Read into the node => Write to the recording server

--- eosclient.js
+++ eosclient.js
@@ -3,7 +3,16 @@ var Db = require('mongodb').Db,
     assert = require('assert'),
     dotenv = require('dotenv');
 
-function process_results(result) {
+async function update_db(client, transactions, marketplace) {
+    
+    const db = client.db("transactions");
+    const write = await db.collection(marketplace).insertOne(transactions);
+
+    console.log("we wrote into the external DB!");
+
+}
+
+async function process_results(result) {
   var nb = result.length;
   var unique_tsion = {};
 
@@ -16,26 +25,36 @@ function process_results(result) {
     unique_tsion[hash.toString()].account = result[i].act.account;
   }
 
+  // We open the connection to the recording database
+  var url = "mongodb://" + process.env.TRANSAC_USR + ":" + process.env.TRANSAC_PWD + "@" + process.env.TRANSAC_SERVER + ":" + process.env.MONGO_DB_PORT + "/";
+  var options = { useNewUrlParser: true };
+  client = await MongoClient.connect(url, options);
+
+  // First we drop all the data to avoid duplicate calls
+  // XXX - TBD
+
   // Index of unique Hashes / 1 hash per unique transaction (as 1 transaction can match multiple actions)
   var hashes = Object.keys(unique_tsion);
   hashes.forEach(function(y) {
     // For each transaction we insert the raw data into our local database
     try {
       //var json = process_memo(unique_tsion[y]);
-      console.log(unique_tsion[y].receiver);
+      update_db(client, unique_tsion[y], "newdexpocket");
 
     } catch(error) {
       console.log(error);
     }
   });
+
+  client.close();
 }
 
-(async function() {
+async function main() {
   console.time("Query time");
   dotenv.load();
   let client;
   try {
-    var url = "mongodb://" + process.env.MONGO_USR + ":" + process.env.MONGO_PWD + "@127.0.0.1:27017/";
+    var url = "mongodb://" + process.env.MONGO_USR + ":" + process.env.MONGO_PWD + "@" + process.env.MONGO_DB_PLUGIN + ":" + process.env.MONGO_DB_PORT + "/";
     var options = { useNewUrlParser: true };
     client = await MongoClient.connect(url, options);
     const db = client.db("EOS");
@@ -51,5 +70,8 @@ function process_results(result) {
   } catch (err) {
     console.log(err.stack);
   }
-  console.timeEnd("Query time");   
-})();  
+  console.timeEnd("Query time");
+}
+
+// Execute the main program
+main();
